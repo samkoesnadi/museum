ce900d7ad9991e21a257629539d7c1e0ff9f4c6f
Samuel <samuel.koesnadi@stud.uni-due.de>
2023-08-26 11:47:17 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+"""
+To upload
+"""
+from datetime import datetime
+import glob
+import os
+from joblib import Parallel, delayed  
+
+import tqdm
+from bs4 import BeautifulSoup
+
+from youtube_upload.client import YoutubeUploader
+
+uploader = YoutubeUploader(secrets_file_path="client_secret_51351923137-4e24fhrvromp18rmf4iuvc3hcidk5eg1.apps.googleusercontent.com.json")
+uploader.authenticate()
+
+VIDEO_PATH = "f:\PRIVATE\M4ROOT\CLIP"
+
+def upload(file_path):
+     # Extract date from xml
+    with open(glob.glob(file_path[:-4] + "*.XML")[0], 'r') as f:
+        xml = f.read()
+    xml_data = BeautifulSoup(xml, "xml")
+    title = xml_data.find("CreationDate").attrs["value"][:-6].replace("T", " ")
+
+    # Video options
+    options = {
+        "title" : title, # The video title
+        "description" : "", # The video description
+        "tags" : ["vlog", "museum-of-sams-life"],
+        "categoryId" : "22",
+        "privacyStatus" : "private", # Video privacy. Can either be "public", "private", or "unlisted"
+        "kids" : False, # Specifies if the Video if for kids or not. Defaults to False.
+        # "thumbnailLink" : "https://cdn.havecamerawilltravel.com/photographer/files/2020/01/youtube-logo-new-1068x510.jpg" # Optional. Specifies video thumbnail.
+    }
+
+    # upload video
+    uploader.upload(file_path, options) 
+
+
+Parallel(n_jobs=2, verbose=10)(upload(file_path) for file_path in tqdm.tqdm(glob.glob(os.path.join(VIDEO_PATH, "*.MP4"))))
+
+uploader.close()
